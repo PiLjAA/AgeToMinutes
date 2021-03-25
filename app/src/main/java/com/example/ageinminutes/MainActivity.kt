8205750d60cd4dad262b136925f428b1ec2ac5a8
Evelin <epiljug1@etf.unsa.ba>
2021-03-25 18:35:40 +0100

    Making a listener on a button and adding a DatePickerDialog

--- app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.ageinminutes
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var button = findViewById(R.id.btnDatePicker) as Button
+
+        button.setOnClickListener{view ->
+            clickDatePicker(view)
+            Toast.makeText(this, "Chose a date",Toast.LENGTH_SHORT).show()
+        }
+
+
+
     }
+
+
+    fun clickDatePicker(view: View){
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(this, DatePickerDialog.OnDateSetListener{view, year, month, dayOfMonth -> }, year, month,day).show()
+    }
+
+
 }
